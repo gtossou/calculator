325f61ad25cd283548552f8ab982405fee91834c
gtossou <gil.tososu@gmail.com>
2020-01-08 23:11:42 +0100

    better front end + calculation with eval function

--- js/calc.js
+++ js/calc.js
@@ -20,12 +20,20 @@ const createCalcZone = function(calcBtnList){
     let resultZone = document.createElement("div")
     resultZone.classList.add("resultZone");
     viewZone.appendChild(resultZone);
-    resultZone.innerText = "resultS";
+    resultZone.innerText = "";
+
+
 
     let inputZone = document.createElement("div")
     inputZone.classList.add("inputZone");
     viewZone.appendChild(inputZone);
-    inputZone.innerText = "input";  
+
+    let inputOperation = document.createElement("input")
+    inputOperation.classList.add("inputOperation");
+    inputOperation.placeholder = "0";
+    //calcBtn.dataset.value = element; 
+    inputZone.appendChild(inputOperation);
+    //inputZone.innerText = "input";  
 
     //left div
     const rightDiv = document.createElement("div");
@@ -45,10 +53,91 @@ const createCalcZone = function(calcBtnList){
 
        let calcBtn = document.createElement("button")
        calcBtn.classList.add("calcBtn");
+       calcBtn.id = `btn_${element}s`;
        calcBtn.innerText = element;
        calcBtn.dataset.value = element; 
-       calcItem.appendChild(calcBtn);   
+       calcItem.appendChild(calcBtn);
+       
+       
     });
 
 } 
 createCalcZone(calcBtnList);
+
+const handleClickOnButton = function(event){
+
+    in_memory = 0;
+    let clickedValue = event.currentTarget;
+    const excepts = ["MR","M+","M-","AC"];
+
+    if (excepts.includes(clickedValue.dataset.value)){
+
+        if (clickedValue.dataset.value === "AC"){
+            document.querySelector(".inputOperation").value = "";
+            document.querySelector(".resultZone").innerText = ""; 
+        }
+
+        if (clickedValue.dataset.value === "M+"){
+
+            function addInMem(){
+
+                const tempMem =  parseFloat(document.querySelector(".resultZone").innerText || "");
+                localStorage.setItem("memory",tempMem);
+
+                // let tempMem = parseFloat(localStorage.getItem("memory"));
+                // if (!tempMem || tempMem === ""){
+                //     tempMem =  parseFloat(document.querySelector(".resultZone").innerText);
+                //     localStorage.setItem("memory",tempMem);
+                // }
+                // else{
+                //     const tempResult = parseFloat(document.querySelector(".resultZone").innerText || "");
+                //     localStorage.setItem("memory",tempMem+tempResult);
+                // }
+                // let tempMem = parseFloat(localStorage.getItem("memory"));
+                
+                
+                console.log(parseFloat(localStorage.getItem("memory") || ""));
+            }
+            return addInMem();
+
+        }
+        if (clickedValue.dataset.value === "M+"){
+            localStorage.clear();
+        }
+        if (clickedValue.dataset.value === "MR"){
+            const tempMem = localStorage.getItem("memory") || "";
+            document.querySelector(".resultZone").innerText = tempMem; 
+        }
+
+    }
+    else if(clickedValue.dataset.value === "="){
+        const inputCurrentValue = document.querySelector(".inputOperation").value;
+        result = eval(inputCurrentValue); //= inputCurrentValue+clickedValue.dataset.value;
+        //document.querySelector(".inputOperation").value = inputCurrentValue;
+        document.querySelector(".resultZone").innerText = result; 
+        console.log(result);
+    }
+    else{
+        let inputCurrentValue = document.querySelector(".inputOperation").value;
+        inputCurrentValue = inputCurrentValue+clickedValue.dataset.value;
+        document.querySelector(".inputOperation").value = inputCurrentValue;
+        console.log(inputCurrentValue)
+    }
+
+    console.log(event.currentTarget);
+}
+
+const handleKeyPress = function(event){
+    console.log(event.currentTarget);
+}
+
+// Interactive Zone click or keypress event 
+const buttonClick = function(){
+    btns = document.querySelectorAll(".calcBtn");
+    console.log(btns);
+    btns.forEach(element => {
+        element.addEventListener("click",handleClickOnButton);
+        element.addEventListener("keypress",handleKeyPress);
+    } )
+}
+buttonClick();
